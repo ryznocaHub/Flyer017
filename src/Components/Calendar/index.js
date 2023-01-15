1c0592199754b7029d5f01c762fbc716fa6ed107
ryznocaHub <fikrihalim27@gmail.com>
2023-01-15 07:15:26 +0700

    add New Component: Calendar

---
+++ src/Components/Calendar/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { createUseStyles } from "react-jss";
+// import DatePicker from 'react-date-picker';
+import { DateTime } from "luxon";
+import { Calendar } from "primereact/calendar";
+
+const CalendarPicker = ({getDate}) => {
+  const classes = useStyles()
+  const minAccept = DateTime.now().plus({ days: 5 }).toISO();
+  const minDate = new Date(minAccept);
+  const maxAccept = DateTime.now().plus({ days: 10, month: 1 }).toISO();
+  const maxDate = new Date(maxAccept);
+
+  return (
+    <Calendar
+      className={classes.calendar}
+      onChange={(e) => getDate(e.value)}
+      inline
+      minDate={minDate}
+      maxDate={maxDate}
+    />
+  );
+};
+
+export default CalendarPicker;
+
+const useStyles = createUseStyles({
+  calendar: {
+    width: "100%",
+    marginBottom: '10px'
+  },
+});
